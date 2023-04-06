6f9f1e0d3a6859f0da3b23b6f3a500056b171aeb
PriyankaBhujade <priyankasbhujade27@gmail.com>
2023-04-06 15:35:24 +0530

    pk

--- src/main/java/com/vgipl/Modal/RopeTypeMaster.java
+++ src/main/java/com/vgipl/Modal/RopeTypeMaster.java
@@ -3,10 +3,23 @@
  */
 package com.vgipl.Modal;
 
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
 /**
  * @author Priyanka Kadukar
  *
  */
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+
 public class RopeTypeMaster {
 
 }
